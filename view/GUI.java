bf073beea3b5b26845fca2046763cc624f2f6af1
Robert <tt47cf6@users.noreply.github.com>
2015-04-02 14:14:49 -0700

    added documentation

--- view/GUI.java
+++ view/GUI.java
@@ -1,4 +1,3 @@
-
 package view;
 
 import java.awt.BorderLayout;
@@ -25,124 +24,162 @@ import javax.swing.SwingUtilities;
 import controller.Calculator;
 import controller.Settings;
 
+/**
+ * This class is the main start class for the application. This creates and
+ * instantiates all the subelements with appropriate error handling for the
+ * serialized global settings object.
+ * 
+ * @author Robert
+ */
 public final class GUI {
-    /** The screen size of the user's machine. Used for centering the window. */
-    public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-    private static final String SETTINGS_SAVE_FILE = "settings.romf";
-    private static final String ICON_FILE = "clover.gif";
-    private static final String CALCULATE = "Calculate";
-    private static final String BACK = "Back";
-    // frame object
-    private final JFrame myFrame;
-    private Settings mySettings;
-    private final CenterPanel myCenterPanel;
-    private final Menu myMenu;
+	/**
+	 * The screen size of the user's machine. Used for centering the window.
+	 */
+	public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit()
+			.getScreenSize();
+	
+	/**
+	 * The filename for serializing the settings.
+	 */
+	private static final String SETTINGS_SAVE_FILE = "settings.romf";
+	
+	/**
+	 * The filename for the icon image for the application.
+	 */
+	private static final String ICON_FILE = "clover.gif";
+	
+	/**
+	 * The button text for starting an analysis.
+	 */
+	private static final String CALCULATE = "Calculate";
+	
+	/**
+	 * The button text for returning to the settings view from the results view.
+	 */
+	private static final String BACK = "Back";
+	
+	/**
+	 * The JFrame for the application.
+	 */
+	private final JFrame myFrame;
+	
+	/**
+	 * The global settings.
+	 */
+	private Settings mySettings;
+	private final CenterPanel myCenterPanel;
+	private final Menu myMenu;
 
-    private GUI() {
-        myFrame = new JFrame();
-        try {
-            final FileInputStream fileIn = new FileInputStream(SETTINGS_SAVE_FILE);
-            final ObjectInputStream in = new ObjectInputStream(fileIn);
-            mySettings = (Settings) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (Exception e) {
-            mySettings = Settings.DEFAULT;
-        }
-        myCenterPanel = new CenterPanel(mySettings);
-        myMenu = new Menu(myFrame, mySettings);
-    }
+	private GUI() {
+		myFrame = new JFrame();
+		try {
+			final FileInputStream fileIn = new FileInputStream(
+					SETTINGS_SAVE_FILE);
+			final ObjectInputStream in = new ObjectInputStream(fileIn);
+			mySettings = (Settings) in.readObject();
+			in.close();
+			fileIn.close();
+		} catch (Exception e) {
+			mySettings = Settings.DEFAULT;
+		}
+		myCenterPanel = new CenterPanel(mySettings);
+		myMenu = new Menu(myFrame, mySettings);
+	}
 
-    private void start() {
-        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        myFrame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(final WindowEvent theEvent) {
-                try {
-                    final FileOutputStream fileOut = new FileOutputStream(SETTINGS_SAVE_FILE);
-                    final ObjectOutputStream out = new ObjectOutputStream(fileOut);
-                    out.writeObject(mySettings);
-                    out.close();
-                    fileOut.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        myFrame.setIconImage(new ImageIcon(ICON_FILE).getImage());
-        // set up card layout
-        myFrame.setJMenuBar(myMenu);
-        myFrame.add(myCenterPanel, BorderLayout.CENTER);
-        final JButton switchButton = new JButton(CALCULATE);
-        switchButton.addActionListener(new SwitchButtonListener());
-        myFrame.add(switchButton, BorderLayout.SOUTH);
-        // start
-        myFrame.pack();
-        centerWindow();
-        myFrame.setResizable(false);
-        myFrame.setVisible(true);
-    }
+	private void start() {
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(final WindowEvent theEvent) {
+				try {
+					final FileOutputStream fileOut = new FileOutputStream(
+							SETTINGS_SAVE_FILE);
+					final ObjectOutputStream out = new ObjectOutputStream(
+							fileOut);
+					out.writeObject(mySettings);
+					out.close();
+					fileOut.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		myFrame.setIconImage(new ImageIcon(ICON_FILE).getImage());
+		// set up card layout
+		myFrame.setJMenuBar(myMenu);
+		myFrame.add(myCenterPanel, BorderLayout.CENTER);
+		final JButton switchButton = new JButton(CALCULATE);
+		switchButton.addActionListener(new SwitchButtonListener());
+		myFrame.add(switchButton, BorderLayout.SOUTH);
+		// start
+		myFrame.pack();
+		centerWindow();
+		myFrame.setResizable(false);
+		myFrame.setVisible(true);
+	}
 
-    /**
-     * Using the constant SCREEN_SIZE, this centers the window on the screen. It
-     * does so by assessing the extra space in both dimensions and then placing
-     * the window where there is an equal amount on either side in both
-     * dimensions.
-     */
-    private void centerWindow() {
-        final int extraVerticalSpace = SCREEN_SIZE.height - myFrame.getHeight();
-        final int extraHorizontalSpace = SCREEN_SIZE.width - myFrame.getWidth();
-        myFrame.setLocation(extraHorizontalSpace / 2, extraVerticalSpace / 2);
-    }
+	/**
+	 * Using the constant SCREEN_SIZE, this centers the window on the screen. It
+	 * does so by assessing the extra space in both dimensions and then placing
+	 * the window where there is an equal amount on either side in both
+	 * dimensions.
+	 */
+	private void centerWindow() {
+		final int extraVerticalSpace = SCREEN_SIZE.height - myFrame.getHeight();
+		final int extraHorizontalSpace = SCREEN_SIZE.width - myFrame.getWidth();
+		myFrame.setLocation(extraHorizontalSpace / 2, extraVerticalSpace / 2);
+	}
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                final GUI gui = new GUI();
-                gui.start();
-            }
-        });
-    }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				final GUI gui = new GUI();
+				gui.start();
+			}
+		});
+	}
 
-    private class SwitchButtonListener implements ActionListener {
+	private class SwitchButtonListener implements ActionListener {
 
-        private static final String SAVE_FILE = "last_results.txt";
+		private static final String SAVE_FILE = "last_results.txt";
 
-        public void actionPerformed(final ActionEvent theEvent) {
-            try {
-                if (!mySettings.anAlgorithmSelected()) {
-                    throw new NullPointerException();
-                }
-                final JButton button = (JButton) theEvent.getSource();
-                if (button.getText().equals(CALCULATE)) {
-                    final String results = Calculator.getResult(mySettings);
-                    saveToFile(results);
-                    myCenterPanel.setResultsText(results);
-                    button.setText(BACK);
-                    myMenu.setButtonsEnabled(false);
-                } else {
-                    button.setText(CALCULATE);
-                    myMenu.setButtonsEnabled(true);
-                }
-                myCenterPanel.nextCard();
-            } catch (FileNotFoundException e) {
-                JOptionPane.showMessageDialog(myFrame, "Database File Not Found!", "Error",
-                                              JOptionPane.ERROR_MESSAGE);
-            } catch (NullPointerException e) {
-                JOptionPane.showMessageDialog(myFrame, "Select the parameters first", "Error",
-                                              JOptionPane.ERROR_MESSAGE);
-            }
-        }
+		public void actionPerformed(final ActionEvent theEvent) {
+			try {
+				if (!mySettings.anAlgorithmSelected()) {
+					throw new NullPointerException();
+				}
+				final JButton button = (JButton) theEvent.getSource();
+				if (button.getText().equals(CALCULATE)) {
+					final String results = Calculator.getResult(mySettings);
+					saveToFile(results);
+					myCenterPanel.setResultsText(results);
+					button.setText(BACK);
+					myMenu.setButtonsEnabled(false);
+				} else {
+					button.setText(CALCULATE);
+					myMenu.setButtonsEnabled(true);
+				}
+				myCenterPanel.nextCard();
+			} catch (FileNotFoundException e) {
+				JOptionPane.showMessageDialog(myFrame,
+						"Database File Not Found!", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			} catch (NullPointerException e) {
+				JOptionPane.showMessageDialog(myFrame,
+						"Select the parameters first", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
 
-        private void saveToFile(final String theResults) {
-            try {
-                final PrintStream out = new PrintStream(SAVE_FILE);
-                out.print(theResults);
-                out.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		private void saveToFile(final String theResults) {
+			try {
+				final PrintStream out = new PrintStream(SAVE_FILE);
+				out.print(theResults);
+				out.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
